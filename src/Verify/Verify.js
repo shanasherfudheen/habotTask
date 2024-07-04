d2175112b9658611863b9c9d4514469076523f5c
Shana Sherfudheen <76563587+shanasherfudheen@users.noreply.github.com>
2024-07-04 17:07:23 +0530

    Add files via upload

---
+++ src/Verify/Verify.js
@@ -0,0 +1,15 @@
+import '../Verify/Verify.css'
+
+function Verify() {
+
+    return(
+        <div className="badge">
+            <h2>Let Suppliers <a href="">Find You</a></h2>
+            <div className="verify-btn">
+                <button>Get Verified</button>
+            </div>
+        </div>
+    )
+}
+
+export default Verify
